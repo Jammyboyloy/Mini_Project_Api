201ad376cc7048220ad40942570ed89c9bb37d1d
Jammyboyloy <jammylay07@gmail.com>
2026-01-01 13:08:54 +0700

    New

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -1,6 +1,22 @@
 // Log out
 let btnLogout = document.querySelector("#btnLogout");
+let btnLogout2 = document.querySelector("#btnLogout2");
 btnLogout.addEventListener("click", () => {
+  fetch(baseUrl + "/auth/logout", {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((item) => {
+      if (item.result) {
+        localStorage.removeItem("token");
+        location.href = "../index.html";
+      }
+    });
+});
+btnLogout2.addEventListener("click", () => {
   fetch(baseUrl + "/auth/logout", {
     method: "DELETE",
     headers: {
